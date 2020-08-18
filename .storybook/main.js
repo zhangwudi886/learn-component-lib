3cf1c712c08b2233fba1edc15c16938a4a5047c0
张世博 <zhangshibo@xdf.cn>
2020-08-18 20:37:57 +0800

    提交第二天测试button组件代码

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,6 +16,15 @@ module.exports = {
       use: [
         {
           loader: require.resolve("react-docgen-typescript-loader"),
+          options: {
+            shouldExtractLiteralValuesFromEnum: true,
+            propFilter: (prop) => {
+              if (prop.parent) {
+                return !prop.parent.fileName.includes("node_modules");
+              }
+              return true;
+            },
+          },
         },
       ],
     });
