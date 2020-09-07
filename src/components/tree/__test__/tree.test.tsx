6ce66fcc065f4c92cca6cadc0f86d0c392473f09
张世博 <zhangshibo@xdf.cn>
2020-09-07 17:12:12 +0800

    tree组件

---
+++ src/components/tree/__test__/tree.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import { Tree } from "../tree";
+import { color, typography } from "../../shared/styles";
+
+describe("test tree component", () => {
+  it(" ", () => {});
+});
