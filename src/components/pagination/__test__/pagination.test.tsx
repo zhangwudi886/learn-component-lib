0aef25530c3c585fc2bab4d282f226382dba97c5
张世博 <zhangshibo@xdf.cn>
2020-09-07 20:43:57 +0800

    pagniation组件

---
+++ src/components/pagination/__test__/pagination.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import { PaginationProps } from "../pagination";
+import { color, typography } from "../../shared/styles";
+
+describe("test pagination component", () => {
+  it(" ", () => {});
+});
