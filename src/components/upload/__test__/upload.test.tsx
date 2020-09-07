ff757c37edb209be58e8534b0eccd2562b8550db
张世博 <zhangshibo@xdf.cn>
2020-09-07 16:40:35 +0800

    upload组件完成

---
+++ src/components/upload/__test__/upload.test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import { Upload } from "../upload";
+import { color, typography } from "../../shared/styles";
+
+describe("test upload component", () => {
+  it(" ", () => {});
+});
